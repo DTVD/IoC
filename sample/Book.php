d0b83d0b06036b9780749eac443434b7e577a1a4
Vu Nhat Minh <vunhat_minh@dwango.co.jp>
2014-02-28 12:52:44 +0900

    add dependencies

---
+++ sample/Book.php
@@ -0,0 +1,37 @@
+<?php
+
+class Book {
+    public $author;
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+}
+
+// Register sample. Here $book1 and $book2 will end up with seperated instances of class Book
+IoC::register('book',function(){
+    $book = new Book;
+    $book->setAuthor('DTVD');
+    return $book;
+});
+
+$book1 = IoC::resolve('book');
+$book2 = IoC::resolve('book');
+echo "Register sample\n";
+var_dump($book1===$book2);
+
+// Singleton sample. Here $bookSingleton1 and $bookSingleton2 will end up with same instances of class Book
+IoC::singleton('bookSingleton',function(){
+    $book = new Book;
+    $book->setAuthor('DTVD');
+    return $book;
+});
+
+$bookSingleton1 = IoC::resolve('bookSingleton');
+$bookSingleton2 = IoC::resolve('bookSingleton');
+echo "Singleton sample\n";
+var_dump($bookSingleton1===$bookSingleton2);
---
+++ src/IoC.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Inversion Of Control Class
+ */
+class IoC {
+  protected static $registry = array();
+  protected static $shared = array();
+
+  // Register
+  public static function register($name, Closure $resolve)
+  {
+     static::$registry[$name] = $resolve;
+  }
+
+  // Singleton
+  public static function singleton($name, Closure $resolve)
+  {
+    static::$shared[$name] = $resolve();
+  }
+
+  // Resolve, consider register or singleton here
+  public static function resolve($name)
+  {
+    if ( static::registered($name) )
+    {
+      $name = static::$registry[$name];
+      return $name;
+    }
+
+    if ( static::singletoned($name) )
+    {
+      $instance = static::$shared[$name];
+      return $instance;
+    }
+
+    throw new Exception('IoC register exception.');
+  }
+
+  // Check resigtered or not
+  public static function registered($name)
+  {
+    return array_key_exists($name, static::$registry);
+  }
+
+  // Check singleton object or not
+  public static function singletoned($name)
+  {
+    return array_key_exists($name, static::$shared);
+  }
+
+}
