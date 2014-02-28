700dd013bd7cde85e2e66e36900701374fe9d4e1
Vu Nhat Minh <vunhat_minh@dwango.co.jp>
2014-02-28 16:17:34 +0900

    seperate file

--- sample/Library.php
+++ sample/Library.php
@@ -1,29 +1,14 @@
 <?php
 
-require_once 'Book.php';
-require_once __DIR__ . '/../vendor/autoload.php';
-
-IoC::register('book_license', function($version){
-  return Book::license($version);
-});
-
-class IoCBook {
-  public static function license($version)
-  {
-    $c = IoC::resolve('book_license');
-    return $c($version);
-  }
-}
+require_once 'IoCConfig.php';
 
 class Library {
   public function overView($version)
   {
-    echo IoCBook::license($version);
+    return IoCBook::license($version);
   }
 }
 
-
 $l = new Library;
 $l->overView("2.0");
-
 
